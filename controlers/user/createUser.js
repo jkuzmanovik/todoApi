6b7242a1c607e4f20e038955dc4338c6b54aa5a2
jkuzmanovik <jordan.kuzmanovik@gmail.com>
2018-03-17 02:38:36 +0100

    added some validation for user, hashed the password

--- controlers/user/createUser.js
+++ controlers/user/createUser.js
@@ -1,8 +1,60 @@
 const User = require ('../../models/schema/userSchema.js')
+const bcrypt  = require('bcryptjs')
+
+var salt = bcrypt.genSaltSync(10);
+
 module.exports = (req,res,next) => {
-    const newUser = new User(req.body)
+
+    var userData = []
+
+    //validate userName
+    if(typeof req.body.userName!== 'string')
+        return res.status(400).send('missing userName')
+    userData.userName = req.body.userName
+
+    //validate email
+    if(typeof req.body.email !== 'string')
+        return res.status(400).send('Missing Email')
+    if(req.body.email)
+        if(!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(req.body.email))
+            return res.status(400).send('Invalid Email')
+        else
+            userData.email = req.body.email
+
+    //validate PhoneNumber
+    if(req.body.phoneNumber && typeof req.body.phoneNumber !=='string')
+        return res.status(400).send('invalid phoneNumber')
+    if(!req.body.phoneNumber)
+        return res.status(400).send('Missing phoneNumber')
+    if(req.body.phoneNumber)
+        userData.phoneNumber = req.body.phoneNumber
+
+    //validate password and hash it
+    if(!req.body.password)
+        return res.status(400).send('Missing password')
+    if(req.body.password && typeof req.body.password === 'string')
+        userData.hash = bcrypt.hashSync(req.body.password, salt);
+
+    //check if firstName is provied and add it to the userData
+    if(req.body.firstName && typeof req.body.firstName !== 'string')
+        return res.status(400).send('invalid firstName')
+    if(req.body.firstName)
+        userData.firstName = req.body.firstName
+    //check if lastName is provided and add it to the userData
+    if(req.body.lastName && typeof req.body.lastName!== 'string')
+        return res.status(400).send('invalid lastName')
+    if(req.body.lastName)
+        userData.lastName = req.body.lastName
+        console.log('doagjam do tuka')
+
+   var newUser = new User(userData)
+
     newUser.save((err,user) => {
-        if(err) return next(err) 
+        if(err) {
+            if(err.code === 11000)
+                return res.status(400).send('Username,Email or Phone alrady used')
+            return next(err)
+        }
         return res.sendStatus(200)
     })
 }
--- models/schema/userSchema.js
+++ models/schema/userSchema.js
@@ -2,16 +2,29 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-    userName:{type:String,required:true,unique:true,trim:true},
-    email:{type:String,required:true,unique:true,trim:true},
-    token:{type:String,default:''},
-    hash:{type:String,required:true},
+    firstName:{type:String,trim:true},
+    lastName:{type:String,trim:true},
+    userName:{type:String,required:true,unique:true,sparse:true,trim:true},
+    email:{type:String,required:true,unique:true,sparse:true,trim:true},
+    phoneNumber:{type:String,unique:true,sparse:true},
+    hash:{type:String,required:true}
 })
 
+// userSchema.pre('save',function(callback) {
+//     if(!this.userName)
+//         return callback(new Error('Missing userName'))
+//     if(!this.email)
+//         return callback(new Error('Missing email'))
+// })
 
-
-
-
+// userSchema.virtual('name').get(function() {
+//     name = "";
+//     if (this.firstName) {
+//         name = this.firstName
+//         if (this.lastName) name += ' ' + this.lastName;
+//     } else if (this.lastName) name = this.lastName;
+//     return name;
+// });
 
 const User = mongoose.model('User',userSchema)
 module.exports = User

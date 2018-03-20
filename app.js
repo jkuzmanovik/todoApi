ea374e0e85dd57341cfe6cc2081ef8dbbd4f0ed6
jkuzmanovik <jordan.kuzmanovik@gmail.com>
2018-03-20 11:02:20 +0100

    working heroku

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost:5000')
 
 //ROUTES
-app.get('/', (req,res,nex) =>  {return res.send('working')})
 app.use(Auth)
 app.use('/user',User)
 
